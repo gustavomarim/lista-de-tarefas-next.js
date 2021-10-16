c7218152c7a1c2ec6cc74069bba1fea9a85afce9
Gustavo Dantas <gustavo.dantas.marim@outlook.com>
2021-10-16 01:02:47 -0300

    Componente Seleção

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,15 @@
-import Tarefa from "../model/Tarefa";
-import tarefasIniciais from "../data/mock";
+import Selecao from "../components/lista/Selecao";
 
 export default function Home() {
-
-  let tarefas = tarefasIniciais;
-  tarefas = tarefas.filtrarAtivas();
-  tarefas = tarefas.filtrarConcluidas();
-  // tarefas = tarefas.removerFiltro();
-  tarefas = tarefas.excluirConcluidas();
-
-  tarefas = tarefas.adicionarTarefa(Tarefa.criarConcluida(6, "Lavar os pratos!!!"));
-  tarefas = tarefas.adicionarTarefa(Tarefa.criarAtiva(7, "Cuidar dos pets"));
-  tarefas = tarefas.excluirConcluidas();
-
-  tarefas = tarefas.modificarTarefa(tarefas.itens[2].alternarStatus());
-  tarefas = tarefas.modificarTarefa(tarefas.itens[1].alternarStatus());
-
-  function renderizarTarefas() {
-    return tarefas.itens.map(tarefa => {
-      return (
-        <div key={tarefa.id}>
-          <span> {tarefa.id} | </span>
-          <span> {tarefa.descricao} | </span>
-          <span> {tarefa.concluida ? "Concluída" : "Ativa"}</span>
-        </div>
-      )
-    })
-  }
 
   return (
     <div className={`
       flex flex-col  justify-center  items-center
-      text-white
-      bg-gradient-to-tr from-purple-500  to-yellow-600
+      text-white  bg-gradient-to-tr from-gray-600  to-gray-900
       h-screen
     `}>
-      {renderizarTarefas()}
+      <Selecao valor={true} />
+      <Selecao valor={false} />
     </div>
   );
 }
